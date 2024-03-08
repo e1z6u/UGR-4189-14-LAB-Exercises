b53f00f07ed146bc73dfd99b75faeb8e93c8f9d9
e1z6u <149933766+e1z6u@users.noreply.github.com>
2024-03-08 15:12:56 +0300

    Exercise 1 answer.

---
+++ LAB-1/LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+// Exercise 1
+
+void main(){
+    String name = "Ezana";
+    int age = 21;
+    String favColor = "blue";
+
+    print("My name is $name. I am $age years old. My favorite color is $favColor.");
+}
