2aa96fac02dda9c3a9ab29033e5d0f199e7eb7cc
e1z6u <149933766+e1z6u@users.noreply.github.com>
2024-03-08 15:46:53 +0300

    Exercise 2 answer.

---
+++ LAB-1/LAB-1-Control-Flow-2.dart
@@ -0,0 +1,9 @@
+// Exercise 2
+
+void main() {
+  // Using a for loop to print the first 10 natural numbers
+  print("The first 10 natural numbers are:");
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
